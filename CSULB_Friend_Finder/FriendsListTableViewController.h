dd951b04abfcc3bcbd67bd28b046b2d0ce770d94
Tan Tran <tankimtran@gmail.com>
2014-10-07 20:16:27 -0700

    friends list functional. gets info from parse now.

---
+++ CSULB_Friend_Finder/FriendsListTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  FriendsListTableViewController.h
+//  CSULB_Friend_Finder
+//
+//  Created by Tan Tran on 10/7/14.
+//  Copyright (c) 2014 Tan Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface FriendsListTableViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
---
+++ CSULB_Friend_Finder/FriendsListTableViewController.m
@@ -0,0 +1,153 @@
+//
+//  FriendsListTableViewController.m
+//  CSULB_Friend_Finder
+//
+//  Created by Tan Tran on 10/7/14.
+//  Copyright (c) 2014 Tan Tran. All rights reserved.
+//
+
+#import "FriendsListTableViewController.h"
+
+@interface FriendsListTableViewController ()
+
+@property (weak, nonatomic) IBOutlet UITableView *myTableView;
+@property NSArray *friendsList;
+
+@end
+
+@implementation FriendsListTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"Are we even in this method?");
+    
+    // Create a programmatic table
+    UITableView *tableView = [[UITableView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame] style:UITableViewStylePlain];
+
+      tableView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleWidth;
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    [tableView reloadData];
+    
+    self.view = tableView;
+    
+    
+    // Load the current user's friends.
+    PFUser *User = [PFUser currentUser];
+    User[@"friends"] = [[NSArray alloc] initWithObjects: @"EeTsYZ2UkU", nil];
+    self.friendsList = User[@"friends"];
+    NSString *firstFriendID = [self.friendsList objectAtIndex:0];
+    
+    PFQuery *query = [PFQuery queryWithClassName:@"_User"]; // Maybe it's just "User"?
+    [query getObjectInBackgroundWithId:firstFriendID block:^(PFObject *theFriend, NSError *error) {
+        if (error)
+        {
+            NSLog(@"Error: %@", error);
+        }
+        else
+        {
+            // Do something with the returned PFObject in the gameScore variable.
+            NSLog(@"The friend was succesfully retrieved. For example, the email was %@", theFriend[@"email"]);
+        }
+    }];
+
+    NSLog(@"here's a booleen from the table view: %i", self.myTableView.showsVerticalScrollIndicator);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+//#warning Potentially incomplete method implementation.
+//    // Return the number of sections.
+//    return 0;
+//}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    NSLog(@"numberOfRowsInSection being called");
+    return self.friendsList.count;
+}
+
+- (NSString*)tableView:(UITableView*) tableView titleForHeaderInSection:(NSInteger)section {
+    NSLog(@"titleForHeaderInSection being called");
+    return @"Test header";
+}
+
+ 
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"cell"];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    }
+
+    NSString *friendId = self.friendsList[indexPath.row];
+    // Make a PFQuery to retrieve the friend with that ID.
+    PFQuery *query = [PFQuery queryWithClassName:@"_User"];
+    [query getObjectInBackgroundWithId:friendId block:^(PFObject *theFriend, NSError *error) {
+        if (error)
+        {
+            NSLog(@"Error: %@", error);
+        }
+        else
+        {
+            NSLog(@"The friend was succesfully retrieved. About to display the friend's email in the table view.");
+            cell.textLabel.text = theFriend[@"email"];
+        }
+    }];
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
