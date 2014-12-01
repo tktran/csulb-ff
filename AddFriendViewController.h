14c9c5ff7b99973470db04049a2b47aadef408bc
Tan Tran <tankimtran@gmail.com>
2014-11-30 16:29:16 -0800

    misc

---
+++ AddFriendViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddFriendViewController.h
+//  CSULB_Friend_Finder
+//
+//  Created by Tan Tran on 11/30/14.
+//  Copyright (c) 2014 Tan Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddFriendViewController : UIViewController
+
+@end
---
+++ AddFriendViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddFriendViewController.m
+//  CSULB_Friend_Finder
+//
+//  Created by Tan Tran on 11/30/14.
+//  Copyright (c) 2014 Tan Tran. All rights reserved.
+//
+
+#import "AddFriendViewController.h"
+
+@interface AddFriendViewController ()
+
+@end
+
+@implementation AddFriendViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
